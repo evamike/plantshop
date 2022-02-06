7a2fa6fd27b059269be2e1e7c276a89a4bff3e2d
evamike <mike.eva.89@gmail.com>
2022-02-06 21:49:36 +0100

    Initial commit

---
+++ src/test/java/com/springcertificationexam/prepbyexample/PlantShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcertificationexam.prepbyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
